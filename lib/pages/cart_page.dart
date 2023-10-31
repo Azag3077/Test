9eae6937c7a4b412abfca19139a6ce000f49988e
toffee23 <460.bakare@gmail.com>
2023-10-30 19:02:57 -0700

    I updated the json and created folders

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,10 +1,44 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ecommerce/components/cart_item.dart';
+import 'package:ecommerce/models/cart.dart';
+import 'package:ecommerce/models/shoe.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class CartPage extends StatelessWidget {
-  const CartPage({Key? key}) : super(key: key);
+  const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Consumer<Cart>(
+      builder: (context, value, child) => Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // heading
+            const Text(
+              'My Cart',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+            ),
+
+            const SizedBox(height: 20),
+
+            Expanded(
+                child: ListView.builder(
+                    itemCount: value.getuserCart().length,
+                    itemBuilder: (context, index) {
+                      // get individual shoe
+                      Shoe individualShoe = value.getuserCart()[index];
+
+                      // return the cart item
+                      return CartItem(shoe: individualShoe);
+                    },
+                    ),
+                    ),
+          ],
+        ),
+      ),
+    );
   }
 }
